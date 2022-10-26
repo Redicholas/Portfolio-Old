22a91be0c10b50921ddb3285fb1c54c04eb4073b
Nicholas Peters <peters.nicholas@gmail.com>
2022-10-26 10:20:20 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    console.log("yum")
+    const menu = document.getElementById("menu");
+
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
